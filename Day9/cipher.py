01d072ad7461075ea83d5e4aae295b4b24016a04
sarmed <sarmed@Ubuntu-22.04.user.virtualbox.org>
2024-07-04 19:38:44 -0400

    day9 complete

--- Day9/cipher.py
+++ Day9/cipher.py
@@ -1,8 +1,39 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def encrypt(text, shift):
+    sol = ""
+    for char in text:
+        if char == " ":
+            sol = sol+(" ")
+        else:
+            i = 0
+            while i <= 25:
+                if alpha[i] == char:
+                    position = i
+                i += 1 
+            new = (position + shift)%26
+            sol = sol+(alpha[new])
+    print(f"encrypted text in rot{shift} is: {sol}")
+
+def decrypt(text, shift):
+    sol = ""
+    for char in text:
+        if char == " ":
+            sol = sol+(" ")
+        else:
+            i = 0
+            while i <= 25:
+                if alpha[i] == char:
+                    position = i
+                i += 1 
+            new = (position - shift)%26
+            sol = sol+(alpha[new])
+    print(f"decrypted text in rot{shift} is: {sol}")
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-    pass
+if (direction == "encode"):
+    encrypt(text,shift)
+elif(direction == "decode"):
+    decrypt(text,shift)
